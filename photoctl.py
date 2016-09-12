e2a7d0703fce3e44433a2e645d87660cf1a0d33e
EdLeafe <ed@leafe.com>
2016-09-12 14:05:17 -0500

    Minor tweaks

--- photoctl.py
+++ photoctl.py
@@ -1,31 +1,35 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
-import os
 import datetime
+import os
+from subprocess import Popen, PIPE
 
 HOMEDIR = "/home/ed/projects/photoviewer"
 DATE_FMT = "%Y.%m.%d %H:%M:%S"
 
+def runproc(cmd):
+    proc = Popen([cmd], shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE,
+            close_fds=True)
+    stdout_text, stderr_text = proc.communicate()
+    return stdout_text, stderr_text
+
+
 now = datetime.datetime.now().strftime(DATE_FMT)
+PHOTODIR = "/home/ed/projects/photoviewer/display"
+VIEWER_CMD = "eog -f -s %s"  % PHOTODIR
 
 with open("/home/ed/.photoctl") as ff:
     action = ff.read().strip()
-os.chdir(HOMEDIR)
-with open("photo.pid") as ff:
-    pid = int(ff.read().strip())
+out, err = runproc("pgrep eog")
 
-running = True
-try:
-    os.kill(pid, 0)
-except OSError:
-    running = False
+running = bool(out)
 
 if running and action == "STOP":
     print(now, "Stopping the app")
-    os.kill(pid, 15)
+    os.kill(int(out), 15)
 elif not running and action == "START":
     print(now, "Starting the app")
-    cmd = "cd %s; /usr/bin/python %s/photo.py & " % (HOMEDIR, HOMEDIR)
-    os.system(cmd)
+    print(now, "CMD", VIEWER_CMD)
+    runproc(VIEWER_CMD)
 
--- togglephoto.py
+++ togglephoto.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-with open(".photoctl", "r") as ff:
+with open("/home/ed/.photoctl", "r") as ff:
     curr = ff.read().strip()
 
 new = "STOP" if curr == "START" else "START"
 
-with open(".photoctl", "w") as ff:
+with open("/home/ed/.photoctl", "w") as ff:
     ff.write(new)
