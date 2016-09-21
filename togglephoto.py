3da5c9af55ccb92f7e0834716843028e8af4e0d2
EdLeafe <ed@leafe.com>
2016-09-21 16:23:52 -0500

    Changed pathing to the pi user

--- togglephoto.py
+++ togglephoto.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-with open("/home/ed/.photoctl", "r") as ff:
+with open("/home/pi/.photoctl", "r") as ff:
     curr = ff.read().strip()
 
 new = "STOP" if curr == "START" else "START"
 
-with open("/home/ed/.photoctl", "w") as ff:
+with open("/home/pi/.photoctl", "w") as ff:
     ff.write(new)
+print(new)
