21059c29dd9dc684db39b167c3207f6874a0e70a
EdLeafe <ed@leafe.com>
2018-03-24 08:11:35 -0500

    Switched to Python3
    Also changed the linux image display program to 'fbi', as 'qiv' was
    having bad flickering issues on the new raspian system.

--- photoctl.py
+++ photoctl.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from __future__ import print_function
 
 import datetime
@@ -18,36 +18,36 @@ def runproc(cmd, wait=True):
         stdout_text, stderr_text = proc.communicate()
         return stdout_text, stderr_text
 
-
 now = datetime.datetime.now().strftime(DATE_FMT)
 PHOTODIR = "%s/display" % HOMEDIR
 #VIEWER_CMD = "eog -f -s %s"  % PHOTODIR
-VIEWER_CMD = "qiv --display :0 -imtfs %s &"  % PHOTODIR
-PYTHON_CMD = "cd %s; python photo.py &" % HOMEDIR
+#VIEWER_CMD = "qiv --display :0 -imtfs %s &"  % PHOTODIR
+VIEWER_CMD = "sudo killall fbi; sudo fbi -a --noverbose --cachemem 0 -T 1 -t 30 %s/display.jpg %s/alias1.jpg %s/alias2.jpg" % (PHOTODIR, PHOTODIR, PHOTODIR)
+PHOTOAPP_CMD = "cd %s; python3 photo.py &" % HOMEDIR
 PID_CMD = "ps -ef | grep %s | grep -v grep | awk '{print $2}'"
-
 
 with open("/home/pi/.photoctl") as ff:
     action = ff.read().strip()
-disp_out, err = runproc(PID_CMD % "qiv")
+#disp_out, err = runproc(PID_CMD % "qiv")
+disp_out, err = runproc(PID_CMD % "fbi")
 disp_running = bool(disp_out)
-python_out, err = runproc(PID_CMD % "photo.py")
-python_running = bool(python_out)
+photoapp_out, err = runproc(PID_CMD % "photo.py")
+photoapp_running = bool(photoapp_out)
 
 if action == "STOP":
     if disp_running:
         print(now, "Stopping the app")
-        os.kill(int(disp_out), 15)
-    if python_running:
+#        os.kill(int(disp_out), 15)
+        runproc("sudo killall fbi")
+    if photoapp_running:
         print(now, "Stopping python controller")
-        os.kill(int(python_out), 15)
+        os.kill(int(photoapp_out), 15)
 elif action == "START":
     if not disp_running:
         print(now, "Starting the app")
         print(now, "CMD", VIEWER_CMD)
         runproc(VIEWER_CMD, False)
-    if not python_running:
+    if not photoapp_running:
         print(now, "Starting python controller")
-        print(now, "CMD", PYTHON_CMD)
-        runproc(PYTHON_CMD, False)
-
+        print(now, "CMD", PHOTOAPP_CMD)
+        runproc(PHOTOAPP_CMD, False)
--- togglephoto.py
+++ togglephoto.py
@@ -1,10 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-with open("/home/pi/.photoctl", "r") as ff:
-    curr = ff.read().strip()
+FNAME = "/home/pi/.photoctl"
+try:
+    with open(FNAME, "r") as ff:
+        curr = ff.read().strip()
+except IOError:
+    # Create the file
+    open(FNAME, "w+")
+    curr = "STOP"
 
 new = "STOP" if curr == "START" else "START"
 
-with open("/home/pi/.photoctl", "w") as ff:
+with open(FNAME, "w") as ff:
     ff.write(new)
 print(new)
